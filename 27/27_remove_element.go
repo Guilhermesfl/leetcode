9ea050dabdf3ccc5eeb4d149caed85615c5ab6e9
guilherme <guilherme@crewmeister.com>
2023-11-26 12:51:45 +0100

    Refactored to reduce space used

--- 27/27_remove_element.go
+++ 27/27_remove_element.go
@@ -3,35 +3,34 @@ package main
 import "fmt"
 
 func removeElement(nums []int, val int) int {
-	size := len(nums)
-	idx_slice := make([]int, size)
-	k := 0
-	for j, i := 0, 0; i < size; i++ {
-		if nums[i] == val {
-			nums[i] = -1
+	i, k := 0, 0
+	j := len(nums) - 1
+	for j >= i {
+		if nums[i] == val && nums[j] == val {
+			nums[j] = -1
+			j--
+			k++
+		} else if nums[i] == val && nums[j] != val {
+			nums[i] = nums[j]
+			nums[j] = -1
+			i++
+			j--
 			k++
 		} else {
-			idx_slice[j] = i
-			j++
+			i++
 		}
-	}
-	idx_slice = idx_slice[:size-k]
-	for j, i := 0, 0; i < size-k; i++ {
-		if nums[i] == -1 {
-			nums[i] = nums[idx_slice[j]]
-			nums[idx_slice[j]] = -1
-		}
-		j++
 	}
 	fmt.Println(nums)
-	return size - k
+	return len(nums) - k
 }
 
 func main() {
-	// nums := [4]int{3, 2, 2, 3}
-	// fmt.Println(removeElement(nums[:], 3))
-	// nums2 := [8]int{0, 1, 2, 2, 3, 0, 4, 2}
-	// fmt.Println(removeElement(nums2[:], 2))
+	nums := [4]int{3, 2, 2, 3}
+	fmt.Println(removeElement(nums[:], 3))
+	nums2 := [8]int{0, 1, 2, 2, 3, 0, 4, 2}
+	fmt.Println(removeElement(nums2[:], 2))
 	nums3 := [3]int{0, 0, 0}
 	fmt.Println(removeElement(nums3[:], 2))
+	nums4 := [1]int{1}
+	fmt.Println(removeElement(nums4[:], 1))
 }
